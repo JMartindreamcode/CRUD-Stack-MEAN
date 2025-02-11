aa9092f6866b5e63af5eb9e3ec55fcc410ab2338
DreamCode <dreamcode@MacBook-Pro-de-DreamCode.local>
2025-02-11 17:07:02 -0500

    crear producto

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app.routes';
+
+// Importación de componentes
+import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,  // <- Aquí está la clave
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-
+import { Routes, RouterModule } from '@angular/router';
+import { CrearProductoComponent } from './componnents/crear-producto/crear-producto.component';
 
 // componentes
-import { ListarProductosComponent } from "./componnents/listar-productos/listar-productos.component";
-import { CrearProductoComponent } from "./componnents/crear-producto/crear-producto.component";
+import { ListarProductosComponent } from './componnents/listar-productos/listar-productos.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: ListarProductosComponent },
